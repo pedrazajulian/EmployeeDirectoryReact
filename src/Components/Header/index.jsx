310d8d259e040b51471d107c9b2ddf5d4f1426b9
julian <pedrazajulian@hotmail.com>
2021-09-21 22:56:44 -0400

    updating naming file

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header>
       <h1 className="text-center">Employee Directory EDR Sytems</h1>
       <p className="text-center">
-        Click on column headers to filter by heading or use the search box to narrow
+        Click on the column headers to filter by category or use the search box to narrow
         your results.
       </p>
     </header>
